0028637a8ea71a6f46b03e69b5d1ecab8cbfdba5
ZY-1026 <1656298063@qq.com>
2020-07-10 22:23:08 +0800

    second

---
+++ src/leetCode/letcode19/Solution.java
@@ -0,0 +1,47 @@
+package leetCode.letcode19;
+
+/**
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * <p>
+ * 示例：
+ * <p>
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * <p>
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int length = 0;
+        ListNode first = head;
+        while (first != null) {
+            length++;
+            first = first.next;
+        }
+        length -= n;
+        first = dummy;
+        while (length > 0) {
+            length--;
+            first = first.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
+    }
+}
