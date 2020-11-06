e503686e8b997e544152e0d66b7cf2c788bc2e44
ZY-1026 <1656298063@qq.com>
2020-11-06 09:47:48 +0800

    20201106

---
+++ src/leetCode/leetcode1207/Solution.java
@@ -0,0 +1,26 @@
+package leetCode.leetcode1207;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        if (arr == null || arr.length == 0) return true;
+        if (arr.length == 1) return true;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        Set<Integer> set = new HashSet<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (set.contains(entry.getValue())) {
+                return false;
+            } else {
+                set.add(entry.getValue());
+            }
+        }
+        return true;
+    }
+}
